59f8fc69200692531e0a915ddf3224c95dbecde6
hasanrahat <rahat.ruet@gmail.com>
2024-09-15 12:21:20 +0600

    visit-country-handler

---
+++ src/Country/Country.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import './Country.css';
+
+const Country=({country})=>{
+const {name,flags,population,area}=country
+const [visited,setVisited]=useState(false);
+const handleVisited=()=>{
+    setVisited(!visited);
+}
+    return(
+       <div className={`country ${visited && "visited"}`}>
+       <h3>Country Name:{name.common}</h3>
+       <img style={{height:160,width:320}}src={flags.png} alt=""/>
+       <p>Country Population:{population}</p>
+       <p>Country Area:{area}</p>
+
+       <button onClick={handleVisited} className="button-visit">{visited? "Visited":"Going"}</button>
+       <br />
+       {visited && "I have visited this country"}
+       </div>
+    );
+}
+
+export default Country;
